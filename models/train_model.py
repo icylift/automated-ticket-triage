c76264b1e56b9565f735530b96d3876687b585fb
Aaron Williams <polarizedcuriosity@gmail.com>
2025-10-20 23:43:32 -0700

    started setting upp the config for sklearn model

--- models/train_model.py
+++ models/train_model.py
@@ -5,4 +5,29 @@ train a simple TF-IDF + logisticRegression pipeline and save to disk
 
 import os
 import pandas as pd
-from sklearn.feature_extraction.txt import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import joblib
+
+# Config
+
+DATA_PATH = os.path.join("..", "data", "sample_tickets.csv")
+MODEL_OUT = os.path.join("..", "models", "triage_pipline.joblib")
+
+def load_data(path):
+  df = pd.read_csv(path)
+  # combine subject + body --> text used for classification
+  df['text'] = df['subject'].fillna('') + " " + df['body'].fillna('')
+  return df
+
+def train_and_save(df):
+  x = df['text']
+  y = df['category']
+
+  #split for quick evaluation
+  X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.2, random_state=42, stratify=y )
+
+  
